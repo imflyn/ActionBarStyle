838d7a2de539de217f3c51c47ecb1ad02cbb7137
imflyn <imflyn@163.com>
2017-03-23 21:05:46 +0800

    Toolbar style

--- app/src/main/java/com/flyn/customactionbarstyle/ToolBarActivity.java
+++ app/src/main/java/com/flyn/customactionbarstyle/ToolBarActivity.java
@@ -1,9 +1,14 @@
 package com.flyn.customactionbarstyle;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ToolBarActivity extends AppCompatActivity {
 
@@ -14,7 +19,29 @@ public class ToolBarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_toolbar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("我是标题");
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ViewCompat.setElevation(findViewById(R.id.appbar), 3);
+        } else {
+            ViewCompat.setBackground(findViewById(R.id.appbar), ContextCompat.getDrawable(this, R.drawable.bg_frame));
+        }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_actionbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
