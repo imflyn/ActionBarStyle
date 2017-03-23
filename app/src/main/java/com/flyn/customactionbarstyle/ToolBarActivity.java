644fc9fd91debd37fa0a18e80936c928c6785088
imflyn <flyn.yu@fandine.com>
2017-03-23 18:06:39 +0800

    create toolbar activity

--- app/src/main/java/com/flyn/customactionbarstyle/ToolBarActivity.java
+++ app/src/main/java/com/flyn/customactionbarstyle/ToolBarActivity.java
@@ -3,6 +3,7 @@ package com.flyn.customactionbarstyle;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class ToolBarActivity extends AppCompatActivity {
 
@@ -10,6 +11,10 @@ public class ToolBarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
 }
